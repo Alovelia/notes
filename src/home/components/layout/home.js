54f2b7733a97c3a21faf9327f88d08def847ea0a
Alovelia <alovelia@mail.ru>
2017-12-19 13:00:18 +0200

    Update layout

--- src/home/components/layout/home.js
+++ src/home/components/layout/home.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import Grid from 'material-ui/Grid';
 import { FormattedMessage } from 'react-intl';
 import Folders from '../../containers/folders';
+import './home.css';
 
 class HomeComponent extends Component {
   state = {
@@ -12,17 +13,21 @@ class HomeComponent extends Component {
     // const spacing = this.state.spacing;
     const { spacing } = this.state; // object destruction
     return (
-      <div style={{ padding: `${spacing / 2}px` }}><Grid container spacing={spacing}>
-        <Grid item xs={4} sm={3}>
-          <Folders />
-        </Grid>
-        <Grid item xs={2} sm={3}>
-          {this.props.notes}
-        </Grid>
-        <Grid item xs={6} sm={6}>
-          {this.props.details}
-        </Grid>
-      </Grid></div>
+      <div style={{ padding: `${spacing / 2}px` }} id="wrapper">
+        <Grid
+          container
+          spacing={spacing}
+        >
+          <Grid item xs={4} sm={3} id="id1">
+            <Folders />
+          </Grid>
+          <Grid item xs={2} sm={3} id="id2">
+            {this.props.notes}
+          </Grid>
+          <Grid item xs={6} sm={6}>
+            {this.props.details}
+          </Grid>
+        </Grid></div>
     );
   }
 }
